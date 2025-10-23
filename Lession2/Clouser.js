d075792e305d3ca2bb5f187de402bcbf90dfdecc
Paban-1 <rpaban89@gmail.com>
2025-10-23 23:09:08 +0530

    Clouser Memory Project(Project): Create a bank account that remembers it's balance

---
+++ Lession2/Clouser.js
@@ -0,0 +1,40 @@
+// Clouser Memory Project
+// Goal: Create a bank account that remembers its balance.
+
+// We need three functions:
+function createAccount(owner) {
+  let balance = 0;
+
+  return {
+    // Create a deposit function
+    deposit(amount) {
+      balance += amount;
+      console.log(`${owner}'s balance is: $${balance}`);
+    },
+
+    // Create a Withdraw function
+    withdraw(amount) {
+      if (amount >= balance) {
+        console.log(
+          `Insufficient funds for ${owner}. Current balance is: $${balance}`
+        );
+      } else {
+        balance -= amount;
+        console.log(`${owner}'s balance is: $${balance}`);
+      }
+    },
+  };
+}
+
+// time to give owner and amount for the account
+// Hold the main function in a variables
+const myAccount = createAccount("Bob");
+
+// Try Deposit
+myAccount.deposit(100); // Bob's balance: $100
+
+// Try Withdraw for then the balance.
+myAccount.withdraw(200); // Insufficient funds for Bob. Current balance is: $100
+
+// Try Withdraw for valid amount
+myAccount.withdraw(50); // Bob's balance: $50
