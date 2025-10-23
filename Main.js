cdeef2ae436f41a2cc07fce870f49f51289002a6
Paban-1 <rpaban89@gmail.com>
2025-10-23 22:36:51 +0530

    Clouser Memory: the memory that will refuses to die

--- Main.js
+++ Main.js
@@ -27,3 +27,21 @@ outer(); // Calling outer function
 // inner() can see everything : z, y, and x
 // but global scope cannot see y and z.
 // This is called lexical scoping - a function can use variables from where it was defined, not nessarily where it is called.
+
+// Closures Memory That Refuses to die
+function createCounter() {
+  let count = 0; // Local Scope Variable this variable lives in clousure memory.
+  return function () {
+    count++; // Incrementing the count variable
+    console.log(count); // Printing the count variable
+  };
+}
+
+const counter1 = createCounter(); // Hold the function in a variable
+counter1(); // Output is : 1
+counter1(); // Output is : 2
+counter1(); // Output is : 3
+
+// Now what happens here ?
+// The count is a inner scope and scence after use the scope the scope dies but here the createCounter function called 3 times so unless the function end use the variable count it will keep alive. but after end of use the count will remove from the memory. or dies.
+// So this will called clouser memory
