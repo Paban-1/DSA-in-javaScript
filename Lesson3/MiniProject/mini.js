9b7067a2c4554b8d545337825c057411ee87c6c2
Paban-1 <rpaban89@gmail.com>
2025-10-27 18:11:20 +0530

    mini Project: Reverse a String Using Recursion

---
+++ Lesson3/MiniProject/mini.js
@@ -0,0 +1,8 @@
+// Mini Project: Reverse a String Using Recursion.
+function reverseString(str) {
+  if (str === "") return ""; // Base case
+return reverseString(str.slice(1)) + str[0]
+}
+
+console.log(reverseString("Lima"));
+
